514201de1e92e9aa4e7ebe3d4a1ecfd8119ac1b3
onewtono <109132211+onewtono@users.noreply.github.com>
2023-09-17 22:04:49 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function app() {
+  return (<View style={{ flex : 1, backgroundColor: "plum", padding : 60 }}>
+    <Text>
+      <Text style={{ color: "white"}}>hello </Text> 
+      world</Text>
+      </View>
+  );
+}
+
